5b6e32a8fb94488fc4decb280af8e656883d137d
emiliarauschert <Emilia.Rauschert@Student.HTW-Berlin.de>
2026-01-10 14:18:15 +0100

    bugs fixen

--- src/router/index.ts
+++ src/router/index.ts
@@ -11,54 +11,41 @@ import Stocks from '@/views/Stocks.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [/*{
-    path: '/',
-    name: 'home',
-    component: HomeAlt
-  },*/
+  routes: [
   {
     path: '/home',
-    name: 'home',
     component: Home
   },
   {
     path: '/Account',
-    name: 'Account',
     component: Account
   },
   {
     path: '/Expenses',
-    name: 'Expenses',
     component: Expenses
   },
   {
     path: '/Income',
-    name: 'Income',
     component: Income
   },
   {
     path: '/Report',
-    name: 'Report',
     component: Report
   },
   {
    path: '/AGB',
-   name: 'AGB',
    component: AGB
   },
   {
    path: '/Impressum',
-   name: 'Impressum',
    component: Impressum
   },
   {
     path: '/Datenschutzerklärung',
-    name: 'Datenschutzerklärung',
     component: Datenschutzerklärung
   },
   {
     path: '/Stocks',
-    name: 'Stocks',
     component: Stocks
   }],
 })
